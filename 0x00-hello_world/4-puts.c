e76d5ceb33e3bedc8d3a523343b48d003162da08
Bless1ing <chinyereblessing2003@gmail.com>
2023-06-19 09:31:40 +0100

    dog

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+4-puts.c
+
+
+#include<stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	        puts("\"Programming is like building a multilingual puzzle");
+
+		        return (0);
+}
