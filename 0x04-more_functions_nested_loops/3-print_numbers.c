d18f33e5c9b5fa49750d5ee1f141994c66820166
Bless1ing <chinyereblessing2003@gmail.com>
2023-06-23 12:06:39 +0100

    drec

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
- */
+ * print_numbers - print the numbers since 0 up to 
+ * Return: The numbers since 0 up to
+ */
+
 void print_numbers(void)
 {
-int c;
+	int x;
 
-for (c = 48; c < 58; c++)
- {
-_putchar(c);
- }
-_putchar('\n');
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0')
+	}
+	_putchar('\n');
 }
